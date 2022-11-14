30eec3caa8bde21a24209328175b19ebcd96cb90
zbndm <zbndmt@gmail.com>
2022-11-14 05:59:18 +0200

    1 file modified

--- pages/api/json.ts
+++ pages/api/json.ts
@@ -1,5 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(_: NextApiRequest, res: NextApiResponse) {
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+
+  await fetch("https://api.logflare.app/api/logs?api_key=9dvsKv4uoB1u&source=09148cf7-b8a4-4f3d-bb83-c20115287dca", {
+            method: "POST",
+            headers: {
+              
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+           
+            log_entry: 'XXXX',
+            metadata: {x:[99]}
+        })
+        })
+
   res.status(200).json({ message: 'hello world!' })
 }
